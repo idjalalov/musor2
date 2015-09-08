5d19a82eae03b26307f6885a3cfeba4ad9a96a3d
idjalalov <ilhomdjalalov@gmail.com>
2015-09-08 17:14:56 -0400

    second commit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by idjalalov on 9/8/2015.
+ */
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.print("HEllo, this is in github");
+
+
+    }
+
+
+}
